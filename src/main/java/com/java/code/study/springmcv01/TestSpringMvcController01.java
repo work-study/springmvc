594f0cf6edfce93cf84dbe94c62cba9ec8ea8dd9
zouwei <zouwei@corp.netease.com>
2019-01-15 14:37:11 +0800

    springmvc国际化

--- src/main/java/com/java/code/study/springmcv01/TestSpringMvcController01.java
+++ src/main/java/com/java/code/study/springmcv01/TestSpringMvcController01.java
@@ -1,8 +1,7 @@
 package com.java.code.study.springmcv01;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,9 +16,14 @@ public class TestSpringMvcController01 {
         System.out.println(user);
         return "success";
     }
-
+    @ModelAttribute
+    public String testModelAttribute(@RequestParam(value = "id",required = false) Integer id,Map<String,Object> map) {
+        System.out.println(id);
+        map.put("key1","value1");
+        return "success";
+    }
     @RequestMapping("/testModelAndView")
-    public ModelAndView testModelAndView(User user) {
+    public ModelAndView testModelAndView(@RequestParam(value = "user",required = false) User user) {
         ModelAndView modelAndView = new ModelAndView("success");
         modelAndView.addObject("time",new Date());
         return modelAndView;
